eccc41b2b3b07f2e3aae0b49455d9a6c1e83230f
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2024-08-07 17:15:14 +0200

    project

---
+++ src/test/java/com/logreapermq/LogReaperMQ/LogReaperMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logreapermq.LogReaperMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogReaperMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
