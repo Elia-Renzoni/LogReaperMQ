9a57a7f898ce457a8703d4c238a196b475bb2eba
unknown <elia.renzoni03@gmail.com>
2024-08-09 10:38:14 +0200

    add subscriber registration handler, to finish brw

---
+++ src/main/java/com/logreapermq/LogReaperMQ/RegistrySystem/Subscriber.java
@@ -0,0 +1,9 @@
+package com.logreapermq.LogReaperMQ.RegistrySystem;
+
+import org.apache.tomcat.util.http.parser.Host;
+
+public class Subscriber {
+    private Host subscriberHost;
+    private Integer subscirberPort;
+    
+}
---
+++ src/main/java/com/logreapermq/LogReaperMQ/Threads/AddTopicAsyncOp.java
@@ -0,0 +1,5 @@
+package com.logreapermq.LogReaperMQ.Threads;
+
+public class AddTopicAsyncOp {
+    
+}
