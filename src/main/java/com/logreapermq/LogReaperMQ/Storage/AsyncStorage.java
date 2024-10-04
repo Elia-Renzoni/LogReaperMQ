03debfa13a264ae2909483b2da327ba0357f3fa7
unknown <elia.renzoni03@gmail.com>
2024-10-04 14:10:27 +0200

    add storage async info

--- src/main/java/com/logreapermq/LogReaperMQ/Storage/AsyncStorage.java
+++ src/main/java/com/logreapermq/LogReaperMQ/Storage/AsyncStorage.java
@@ -1,10 +1,20 @@
 package com.logreapermq.LogReaperMQ.Storage;
 
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
 public class AsyncStorage {
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor storageTaskExecutor() {
+        ThreadPoolTaskExecutor tPool = new ThreadPoolTaskExecutor();
+        return tPool;
+    }
     
 }
